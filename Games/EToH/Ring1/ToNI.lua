62388655d8c2a69f755a8a08278e85c0e132346a
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-05-28 20:21:08 +0800

    Update ToNI.lua

--- Games/EToH/Ring1/ToNI.lua
+++ Games/EToH/Ring1/ToNI.lua
@@ -1,6 +1,29 @@
 return function()
     return {
         workspace.Towers.ToNI.Obby:GetChildren()[10],
+        workspace.Towers.ToNI.Obby:GetChildren()[15],
+        workspace.Towers.ToNI.Obby:GetChildren()[27],
+        workspace.Towers.ToNI.Obby:GetChildren()[33],
+        workspace.ClientParts.DistanceAnchoringObjects:GetChildren()[8]:GetChildren()[2],
+        workspace.ClientParts.DistanceAnchoringObjects:GetChildren()[6]:GetChildren()[2],
+        workspace.Towers.ToNI.Obby:GetChildren()[35],
+        workspace.ClientParts["Kill Bricks"]:GetChildren()[7],
+        workspace.ClientParts["Kill Bricks"]:GetChildren()[8],
+        workspace.Towers.ToNI.Obby:GetChildren()[40],
+        workspace.Towers.ToNI.Obby:GetChildren()[41],
+        workspace.Towers.ToNI.Obby:GetChildren()[45],
+        workspace.Towers.ToNI.Obby:GetChildren()[59],
+        workspace.Towers.ToNI.Obby:GetChildren()[55],
+        workspace.ClientParts.DistanceAnchoringObjects.Spinner.Part,
+        workspace.Towers.ToNI.Obby:GetChildren()[8],
+        workspace.Towers.ToNI.Obby:GetChildren()[95],
+        workspace.Towers.ToNI.Obby:GetChildren()[101],
+        workspace.Towers.ToNI.Obby:GetChildren()[104],
+        workspace.ClientParts.DistanceAnchoringObjects:GetChildren()[10]:GetChildren()[2],
+        workspace.Towers.ToNI.Obby:GetChildren()[87],
+        workspace.Towers.ToNI.Obby:GetChildren()[91],
+        workspace.Towers.ToNI.Obby:GetChildren()[92]
+        
     
     }
 end
