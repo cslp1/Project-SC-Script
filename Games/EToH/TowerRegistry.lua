5dacd88e7660c1a212e09ef2cc51f6a5457d01bd
MaybeIsRealZack <maybeisrealzack@gmail.com>
2026-06-01 17:34:50 +0800

    Create TowerRegistry.lua

---
+++ Games/EToH/TowerRegistry.lua
@@ -0,0 +1,38 @@
+local Categories = {
+    Ring1 = 9070657865,
+    Ring2 = 9070979698,
+}
+
+return {
+    Categories = Categories,
+    Towers = {
+        { name = "NEAT",  category = "Ring1", suggestedTime = { min = "0", sec = "5" } },
+        { name = "ToAST", category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToA",   category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToM",   category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToNI",  category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToH",   category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToK",   category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToKY",  category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToS",   category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToSP",  category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToSP:SE", category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToR",   category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToIE",  category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "CoLS",  category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "CoLS:SE", category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToTS",  category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToIG",  category = "Ring1", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToPS",  category = "Ring2", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToBH",  category = "Ring2", suggestedTime = { min = "3", sec = "0" } },
+        { name = "ToOH",  category = "Ring2", suggestedTime = { min = "3", sec = "0" } },
+    },
+    TowerRush = {
+        {
+            name        = "R1TR",
+            category    = "Ring1",
+            suggestedTime = { min = "39", sec = "0" },
+            isTowerRush = true,
+        },
+    },
+}
